ec5343586adf7cdb71fb355594c946e5ec8043d3
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:43:48 +0530

    appwrite init

--- src/shared/services/appwrite.tsx
+++ src/shared/services/appwrite.tsx
@@ -1,13 +1,15 @@
-import { Account, Client, ID } from "appwrite";
+import { Account, Client, Databases, ID, Permission } from "appwrite";
 
 const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string;
 const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string;
 
 const client = new Client();
-console.log({ endpoint, projectId });
+
 if (endpoint && projectId) {
   client.setEndpoint(endpoint).setProject(projectId);
 }
-const account = new Account(client);
 
-export { client, account, ID };
+const account = new Account(client);
+const database = new Databases(client);
+
+export { client, account, ID, database, Permission };
