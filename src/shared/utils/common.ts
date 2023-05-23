e3eefe0c67f8d68c5994e0b47c8272c0548f13ba
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:44:12 +0530

    added capitalize util

--- src/shared/utils/common.ts
+++ src/shared/utils/common.ts
@@ -14,3 +14,8 @@ export const isBrowser = canUseDOM();
 export function getPathUrl() {
     return window.parent.location.href.replace(/.*\/\/[^\/]*/, '');
 }
+
+// write a function to capitalize a string
+export function capitalize(str: string) {
+    return str.replace(/^\w/, c => c.toUpperCase());
+}
