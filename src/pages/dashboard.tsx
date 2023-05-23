dd60dc4853644bb8009467a52a6225acdbb139d7
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:48:55 +0530

    dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import CategoriesPieChart from "expensasaures/components/dashboard/categorypiechart";
+import LineChartTabs from "expensasaures/components/dashboard/linechart";
+import DashboardStatistics from "expensasaures/components/dashboard/stats";
+import Layout from "expensasaures/components/layout/Layout";
+import useDashboard from "expensasaures/hooks/useDashboard";
 
 const dashboard = () => {
-  return <div className="font-light">dashboard</div>;
+  const {
+    statistics,
+    incomeThisMonth,
+    expenseThisMonth,
+    expensesAndPercentByCategoryThisMonth,
+  } = useDashboard();
+
+  return (
+    <Layout>
+      <main className="dark:bg-navy-900 min-h-[100vh]">
+        <div className="max-w-[1200px] mx-auto pt-10">
+          <DashboardStatistics stats={statistics} />
+          <div className="flex justify-between gap-4 mt-8">
+            <div className="w-[66%]">
+              <LineChartTabs />
+            </div>
+            <div className="w-[32%]">
+              <CategoriesPieChart
+                incomeThisMonth={incomeThisMonth}
+                expensesAndPercentByCategoryThisMonth={
+                  expensesAndPercentByCategoryThisMonth
+                }
+                expenseThisMonth={expenseThisMonth}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
 };
 
 export default dashboard;
