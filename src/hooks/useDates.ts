6dc5acecdb3b74bd2f0a0babea2fd6712fc15564
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 14:38:39 +0530

    form utils

--- src/hooks/useDates.ts
+++ src/hooks/useDates.ts
@@ -27,7 +27,6 @@ const useDates = () => {
         endOfThisMonth,
         startOfEarlierMonth,
         endOfEarlierMonth
-
     }
 
 }
--- src/shared/utils/form.ts
+++ src/shared/utils/form.ts
@@ -1,3 +1,34 @@
+import { MutableState, Tools } from 'final-form';
+
+type mutatorsType = (
+    [name, value]: any,
+    state: MutableState<object, object>,
+    utils: Tools<object, object>
+) => void;
+
+export const setError: mutatorsType = ([fieldName, error], state) => {
+    if (error !== undefined) {
+        if (state.formState.errors?.[fieldName]) {
+            state.formState.errors[fieldName] = error;
+        } else {
+            state.formState.errors = {
+                ...state.formState.errors,
+                [fieldName]: error
+            };
+        }
+
+    } else {
+        delete state.formState.errors?.[fieldName];
+    }
+};
+
+export const setFieldValue: mutatorsType = ([name, value], state, utils) => {
+    utils.changeValue(state, name, () => value);
+};
+
+export const defaultMutators = { setError, setFieldValue };
+
+
 export const validateExpenseForm = (values: any) => {
     let errors: any = {};
 
@@ -25,5 +56,10 @@ export const validateExpenseForm = (values: any) => {
         errors.currency = "Currency is required";
     }
 
+    return errors;
+}
+
+export const validateBudgetForm = (values: any) => {
+    const errors: any = {};
     return errors;
 }
