f375663431455d6ccb76c8eafe2f95441e95be58
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:49:30 +0530

    calender page

---
+++ src/components/calender/ExpenseList.tsx
@@ -0,0 +1,76 @@
+import { Models, Query } from "appwrite";
+import { format, isSameDay, isTomorrow } from "date-fns";
+import { getAllLists } from "expensasaures/shared/services/query";
+import { useAuthStore } from "expensasaures/shared/stores/useAuthStore";
+import { Transaction } from "expensasaures/shared/types/transaction";
+import { shallow } from "zustand/shallow";
+import ExpenseCalCard from "./ExpenseCalCard";
+
+interface Props {
+  selectedDay: Date;
+  //   data: DocumentListType<Transaction> | undefined;
+}
+
+const ExpenseList = (props: Props) => {
+  const { selectedDay } = props;
+  const { user } = useAuthStore((state) => ({ user: state.user }), shallow) as {
+    user: Models.Session;
+  };
+
+  const startOfDay = new Date(
+    selectedDay.getFullYear(),
+    selectedDay.getMonth(),
+    selectedDay.getDate()
+  ).toISOString();
+
+  const endOfDay = new Date(
+    selectedDay.getFullYear(),
+    selectedDay.getMonth(),
+    selectedDay.getDate() + 1
+  ).toISOString();
+
+  const { data, isLoading } = getAllLists<Transaction>(
+    ["Expenses", user?.userId, selectedDay],
+    [
+      "6467f9811c14ca905ed5",
+      "6467f98b8e8fe5ffa576",
+      [
+        Query.equal("userId", user?.userId),
+        Query.lessThanEqual("date", endOfDay),
+        Query.greaterThan("date", startOfDay),
+      ],
+    ],
+    { enabled: !!user, staleTime: Infinity }
+  );
+  return (
+    <>
+      <h2 className="font-semibold text-gray-900">
+        Expenses for{" "}
+        <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+          {format(selectedDay, "MMM dd, yyy")}
+        </time>
+      </h2>
+      <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+        {isLoading ? (
+          <>loading...</>
+        ) : data?.documents && data?.documents?.length > 0 ? (
+          data?.documents?.map((data, index) => (
+            <ExpenseCalCard meeting={data} key={index} />
+          ))
+        ) : (
+          <p>
+            No expenses listed for{" "}
+            {isSameDay(selectedDay, new Date())
+              ? "today"
+              : isTomorrow(selectedDay)
+              ? "tomorrow"
+              : format(selectedDay, "MMM dd, yyy")}
+            .
+          </p>
+        )}
+      </ol>
+    </>
+  );
+};
+
+export default ExpenseList;
---
+++ src/pages/calender.tsx
@@ -0,0 +1,146 @@
+import clsx from "clsx";
+import {
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parseISO,
+} from "date-fns";
+import ExpenseList from "expensasaures/components/calender/ExpenseList";
+import Layout from "expensasaures/components/layout/Layout";
+import { useCalender } from "expensasaures/hooks/useCalender";
+import { MdOutlineChevronLeft, MdOutlineChevronRight } from "react-icons/md";
+
+const Calender = () => {
+  const {
+    firstDayCurrentMonth,
+    previousMonth,
+    nextMonth,
+    days,
+    setSelectedDay,
+    selectedDay,
+    thisMonthExpenses,
+  } = useCalender();
+
+  return (
+    <Layout>
+      <div className="pt-16">
+        <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+          <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+            <div className="md:pr-14">
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-gray-900">
+                  {format(firstDayCurrentMonth, "MMMM yyyy")}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Previous month</span>
+                  <MdOutlineChevronLeft
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                  />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <MdOutlineChevronRight
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                  />
+                </button>
+              </div>
+              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500 dark:text-white">
+                <div>S</div>
+                <div>M</div>
+                <div>T</div>
+                <div>W</div>
+                <div>T</div>
+                <div>F</div>
+                <div>S</div>
+              </div>
+              <div className="grid grid-cols-7 mt-2 text-sm">
+                {days.map((day, dayIdx) => (
+                  <div
+                    key={day.toString()}
+                    className={clsx(
+                      dayIdx === 0 && colStartClasses[getDay(day)],
+                      "py-1.5"
+                    )}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setSelectedDay(day);
+                      }}
+                      className={clsx(
+                        isEqual(day, selectedDay) && "text-white",
+                        !isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "text-red-500",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-900",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-400",
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "bg-red-500",
+                        isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          "bg-gray-900",
+                        !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                        (isEqual(day, selectedDay) || isToday(day)) &&
+                          "font-semibold",
+                        "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      )}
+                    >
+                      <time dateTime={format(day, "yyyy-MM-dd")}>
+                        {format(day, "d")}
+                      </time>
+                    </button>
+
+                    <div className="w-1 h-1 mx-auto mt-1">
+                      {thisMonthExpenses?.documents?.some((meeting) =>
+                        isSameDay(parseISO(meeting.date), day)
+                      ) && (
+                        <div className="w-1 h-1 rounded-full bg-blue-500">
+                          &nbsp;
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <section className="mt-12 md:mt-0 md:pl-14">
+              <ExpenseList selectedDay={selectedDay} />
+            </section>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+let colStartClasses = [
+  "",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+];
+
+export default Calender;
