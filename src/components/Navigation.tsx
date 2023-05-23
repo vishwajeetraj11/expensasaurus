42542aa780ad17e5c601e02f11f93f29cce94062
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:45:43 +0530

    navigation added

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Navigation = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto flex items-center justify-between">
+      <div>Logo</div>
+      <div className="flex gap-4">
+        <Link href={"/dashboard"}>Dashboard</Link>
+        <Link href={"/expenses"}>Expense</Link>
+        <Link href={"/category"}>Category</Link>
+        <Link href={"/calender"}>Calender</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
