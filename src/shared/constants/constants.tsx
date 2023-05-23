ab3dbbf686be4050c571e771e6d2f8a49f5c05dd
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-23 10:47:17 +0530

    constants

---
+++ src/shared/constants/constants.tsx
@@ -0,0 +1,9 @@
+import { Color } from "@tremor/react";
+
+export const dashboardStatColors: { [key: string]: Color } = {
+  increase: "emerald",
+  moderateIncrease: "emerald",
+  unchanged: "orange",
+  moderateDecrease: "rose",
+  decrease: "rose",
+};
