5e76c67e5390077a98ff4963433896341dbf7351
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-26 00:14:18 +0530

    expense cal card delete functionality

--- src/components/calender/ExpenseCalCard.tsx
+++ src/components/calender/ExpenseCalCard.tsx
@@ -1,9 +1,11 @@
+import { TrashIcon } from "@heroicons/react/outline";
 import clsx from "clsx";
 import { formatDistance } from "date-fns";
 import { categories } from "expensasaures/shared/constants/categories";
+import { database } from "expensasaures/shared/services/appwrite";
 import { Transaction } from "expensasaures/shared/types/transaction";
 import { timeSince } from "expensasaures/shared/utils/dates";
-
+import { toast } from "sonner";
 interface Props {
   expense: Transaction;
 }
@@ -17,6 +19,18 @@ function ExpenseCalCard(props: Props) {
     (category) => category.key === expense.category
   );
   const SelectedIcon = categoryInfo?.Icon;
+  const onDelete = async (expenseId: string) => {
+    try {
+      const data = await database.deleteDocument(
+        "6467f9811c14ca905ed5",
+        "6467f98b8e8fe5ffa576",
+        expenseId
+      );
+      toast.success("Expense deleted successfully");
+    } catch (error) {
+      toast.error("Expense deletion failed.");
+    }
+  };
   return (
     <li className="flex px-4 py-2 items-center rounded-xl transition-all duration-300 hover:bg-slate-100/80 relative">
       <div className="py-2 flex flex-1">
@@ -39,9 +53,17 @@ function ExpenseCalCard(props: Props) {
             {expense.description}
           </p>
         </div>
-        <p className="text-xs ml-auto text-slate-500 text-right">
-          {timeSince(createdDate) || formattedDate}
-        </p>
+        <div className="ml-auto flex flex-col">
+          <p className="text-xs  text-slate-500 text-right">
+            {timeSince(createdDate) || formattedDate}
+          </p>
+          <div
+            onClick={(e) => onDelete(expense.$id)}
+            className="bg-red-500/30 cursor-pointer mt-auto ml-auto mt-auto w-5 h-5 rounded-full flex items-center justify-center"
+          >
+            <TrashIcon className="text-red-500 w-3 h-3" />
+          </div>
+        </div>
       </div>
     </li>
   );
