a05348ec8a8d8e10022051069b7569bba11e7007
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-26 00:12:02 +0530

    nav ui improvement

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import { Logo } from "./icons/svg";
 
 const Navigation = () => {
   return (
-    <div className="max-w-[1200px] mx-auto flex items-center justify-between">
-      <div>Logo</div>
-      <div className="flex gap-4">
-        <Link href={"/dashboard"}>Dashboard</Link>
-        <Link href={"/expenses"}>Expense</Link>
-        <Link href={"/category"}>Category</Link>
-        <Link href={"/calender"}>Calender</Link>
-        <Link href={"/budgets"}>Budget</Link>
+    <div className=" ">
+      <div className="max-w-[1200px] mx-auto py-2 flex items-center justify-between ">
+        <div>
+          <Logo width={40} height={40} />
+        </div>
+        <div className="flex gap-4">
+          <Link href={"/dashboard"}>
+            <p className="px-4 py-2 rounded-full">Dashboard</p>
+          </Link>
+          <Link href={"/expenses"}>
+            <p className="px-4 py-2 rounded-full">Expense</p>
+          </Link>
+          <Link href={"/category"}>
+            <p className="px-4 py-2 rounded-full">Category</p>
+          </Link>
+          <Link href={"/calender"}>
+            <p className="px-4 py-2 rounded-full">Calender</p>
+          </Link>
+          <Link href={"/budgets"}>
+            <p className="px-4 py-2 rounded-full">Budget</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
