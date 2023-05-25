6ab3c431f565cc18f637be9faf993b934a94f064
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-26 00:13:42 +0530

    constants

--- src/shared/constants/constants.tsx
+++ src/shared/constants/constants.tsx
@@ -11,3 +11,14 @@ export const dashboardStatColors: { [key: string]: Color } = {
 export const regex = {
   number: /^[0-9\b]+$/,
 };
+
+export const ENVS = {
+  COLLECTIONS: {
+    EXPENSES: process.env.NEXT_PUBLIC_EXPENSES_COLLECTION_ID as string,
+    INCOMES: process.env.NEXT_PUBLIC_INCOMES_COLLECTION_ID as string,
+    BUDGETS: process.env.NEXT_PUBLIC_BUDGETS_COLLECTION_ID as string,
+  },
+  DB_ID: process.env.NEXT_PUBLIC_DATABASE_ID as string,
+  PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+  APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string,
+};
