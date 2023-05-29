a4a669952b0d84162a003b9f45bc8ffb52c2c335
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-29 16:30:17 +0530

    added delete modal

---
+++ src/components/modal/DeleteModal.tsx
@@ -0,0 +1,32 @@
+import { capitalize } from "expensasaures/shared/utils/common";
+import EModal from ".";
+
+interface Props {
+  isOpen: boolean;
+  resource: "expense";
+  onClose: () => void;
+  action: "delete";
+  onAction: () => void;
+}
+
+const DeleteModal = (props: Props) => {
+  const { isOpen, onClose, resource, action, onAction } = props;
+  //   const isExpense = resource === "expense";
+  const title = `Delete ${capitalize(resource)}`;
+  const isActionDelete = action === "delete";
+  const message = `Are you sure you want to delete this ${resource}? Deleting this ${resource} will remove it from your records and cannot be undone.`;
+
+  return (
+    <EModal
+      onAction={onAction}
+      title={title}
+      action="delete"
+      description={message}
+      primaryCtaText="Delete"
+      isOpen={isOpen}
+      onClose={onClose}
+    />
+  );
+};
+
+export default DeleteModal;
