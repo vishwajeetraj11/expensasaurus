32a731354c31cf1207c35c2c0a39695ca5908f8f
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-05-29 16:36:20 +0530

    making expenses card a link to expense by id page

--- src/components/calender/ExpenseCalCard.tsx
+++ src/components/calender/ExpenseCalCard.tsx
@@ -2,9 +2,11 @@ import { TrashIcon } from "@heroicons/react/outline";
 import clsx from "clsx";
 import { formatDistance } from "date-fns";
 import { categories } from "expensasaures/shared/constants/categories";
+import { ENVS } from "expensasaures/shared/constants/constants";
 import { database } from "expensasaures/shared/services/appwrite";
 import { Transaction } from "expensasaures/shared/types/transaction";
 import { timeSince } from "expensasaures/shared/utils/dates";
+import Link from "next/link";
 import { toast } from "sonner";
 interface Props {
   expense: Transaction;
@@ -19,10 +21,11 @@ function ExpenseCalCard(props: Props) {
     (category) => category.key === expense.category
   );
   const SelectedIcon = categoryInfo?.Icon;
+
   const onDelete = async (expenseId: string) => {
     try {
       const data = await database.deleteDocument(
-        "6467f9811c14ca905ed5",
+        ENVS.DB_ID,
         "6467f98b8e8fe5ffa576",
         expenseId
       );
@@ -31,41 +34,44 @@ function ExpenseCalCard(props: Props) {
       toast.error("Expense deletion failed.");
     }
   };
-  return (
-    <li className="flex px-4 py-2 items-center rounded-xl transition-all duration-300 hover:bg-slate-100/80 relative">
-      <div className="py-2 flex flex-1">
-        {SelectedIcon && (
-          <div
-            className={clsx(
-              "min-w-[40px] h-10 bg-opacity-25 rounded-full flex items-center justify-center mr-3",
-              categoryInfo.className
-            )}
-          >
-            <SelectedIcon className="w-5 h-5" />
-          </div>
-        )}
 
-        <div className="w-[65%]">
-          <p className="text-stone-700 font-semibold text-md">
-            {expense.title}
-          </p>
-          <p className="text-stone-600 text-xs font-medium line-clamp-1">
-            {expense.description}
-          </p>
-        </div>
-        <div className="ml-auto flex flex-col">
-          <p className="text-xs  text-slate-500 text-right">
-            {timeSince(createdDate) || formattedDate}
-          </p>
-          <div
-            onClick={(e) => onDelete(expense.$id)}
-            className="bg-red-500/30 cursor-pointer mt-auto ml-auto mt-auto w-5 h-5 rounded-full flex items-center justify-center"
-          >
-            <TrashIcon className="text-red-500 w-3 h-3" />
+  return (
+    <Link href={`/expenses/${expense.$id}`}>
+      <li className="flex px-4 py-2 items-center rounded-xl transition-all duration-300 hover:bg-slate-100/80 relative">
+        <div className="py-2 flex flex-1">
+          {SelectedIcon && (
+            <div
+              className={clsx(
+                "min-w-[40px] h-10 bg-opacity-25 rounded-full flex items-center justify-center mr-3",
+                categoryInfo.className
+              )}
+            >
+              <SelectedIcon className="w-5 h-5" />
+            </div>
+          )}
+
+          <div className="w-[65%]">
+            <p className="text-stone-700 font-semibold text-md">
+              {expense.title}
+            </p>
+            <p className="text-stone-600 text-xs font-medium line-clamp-1">
+              {expense.description}
+            </p>
+          </div>
+          <div className="ml-auto flex flex-col">
+            <p className="text-xs  text-slate-500 text-right">
+              {timeSince(createdDate) || formattedDate}
+            </p>
+            <div
+              onClick={(e) => onDelete(expense.$id)}
+              className="bg-red-500/30 cursor-pointer mt-auto ml-auto w-5 h-5 rounded-full flex items-center justify-center"
+            >
+              <TrashIcon className="text-red-500 w-3 h-3" />
+            </div>
           </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 }
 
