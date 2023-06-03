29996e2a6262f7b5af2d3cada3c28bb160fc4427
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-03 16:59:35 +0530

    padding text are

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React, { HTMLProps } from "react";
+import { HTMLProps } from "react";
 
 interface TextAreaProps extends HTMLProps<HTMLTextAreaElement> {
   message?: string;
@@ -9,7 +9,7 @@ interface TextAreaProps extends HTMLProps<HTMLTextAreaElement> {
 const TextArea = (props: TextAreaProps) => {
   const { message, error, ...rest } = props;
   const defaultClasses = clsx(
-    "block w-full focus:outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    "block w-full focus:outline-none rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
   );
 
   return (
@@ -18,8 +18,8 @@ const TextArea = (props: TextAreaProps) => {
         className={defaultClasses}
         rows={3}
         {...rest}
-        // message={meta.touched && meta.error}
-        // state={meta.error && meta.touched ? "error" : "idle"}
+      // message={meta.touched && meta.error}
+      // state={meta.error && meta.touched ? "error" : "idle"}
       />
       {message && (
         <span className={clsx(error ? "text-red-600" : "text-stone-700")}>
