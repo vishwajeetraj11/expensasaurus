a58099f6c60979281f2801eae57b8c522fd92255
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-03 16:59:14 +0530

    info

---
+++ src/shared/constants/emptyState.tsx
@@ -0,0 +1,6 @@
+const noMoreExpensesInCategory = {
+    title: 'No More Expenses in this Category',
+    description: 'You have explored all the expenses in this category. Start adding new expenses to continue tracking your spending.',
+    buttonText: 'Add New Expense',
+    buttonLink: '/expenses/create',
+}
