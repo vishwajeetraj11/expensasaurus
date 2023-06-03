8af8450f5f8bde9f5a3fa4b89218e57783832453
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-03 16:58:43 +0530

    file preview disabled support

--- src/components/expense/expenseForm/FilePreview.tsx
+++ src/components/expense/expenseForm/FilePreview.tsx
@@ -7,17 +7,18 @@ import { MdClose } from 'react-icons/md';
 interface Props {
     file: FileWPreview;
     setFiles: React.Dispatch<React.SetStateAction<FileWPreview[]>>;
+    disabled?: boolean;
 }
 
 export const ImageFilePreview = (props: Props) => {
-    const { file, setFiles } = props;
+    const { file, setFiles, disabled = false } = props;
     return (
         <div className="p-3 box-shadow-card relative">
-            <span onClick={() => {
+            <button type='button' disabled={disabled} onClick={() => {
                 setFiles(files => files.filter(f => f.preview !== file.preview))
             }} className="top-[-10px] flex items-center group transition-all duration-200 justify-center right-[-10px] hover:bg-red-600 absolute bg-white p-2 rounded-full border border-[#f4f4f4] h-[30px] w-[30px] cursor-pointer">
                 <MdClose className="h-[12px]  group-hover:text-white" />
-            </span>
+            </button>
             {file.preview && <Image src={file.preview} height={100} width={100} className="w-[100px] h-[100px] object-cover" alt={file?.file?.name} />}
         </div>
     )
@@ -25,15 +26,15 @@ export const ImageFilePreview = (props: Props) => {
 
 
 export const FIlePreview = (props: Props) => {
-    const { file, setFiles } = props;
+    const { file, setFiles, disabled = false } = props;
 
     return <div>
         <div className="h-[100px] w-[100px] flex relative p-3 box-shadow-card">
-            <span onClick={() => {
+            <button disabled={disabled} onClick={() => {
                 setFiles(files => files.filter(f => f.file.name !== file.file.name))
             }} className="top-[-10px] flex items-center group transition-all duration-200 justify-center right-[-10px] hover:bg-red-600 absolute bg-white p-2 rounded-full border border-[#f4f4f4] h-[30px] w-[30px] cursor-pointer">
                 <MdClose className="h-[12px] group-hover:text-white" />
-            </span>
+            </button>
             <div className="bg-slate-400 flex flex-1">
                 <FcFile className="self-center flex flex-1" />
             </div>
