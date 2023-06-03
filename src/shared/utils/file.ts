f0678631ab0426f50a5a0b25690f8b64b584831c
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-03 10:51:34 +0530

    added utils for file

---
+++ src/shared/utils/file.ts
@@ -0,0 +1,13 @@
+export const getBase64 = (file: File) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+    });
+}
+
+export const isUploadedFileImage = (file: File) => {
+    const imageTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    return file && imageTypes.includes(file.type);
+}
