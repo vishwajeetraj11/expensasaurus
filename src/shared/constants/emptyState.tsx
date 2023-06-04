b1e01a90574b19c9c5609fd103c53b533ee6c5ed
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:04:31 +0530

    empty state constants

--- src/shared/constants/emptyState.tsx
+++ src/shared/constants/emptyState.tsx
@@ -1,6 +1,13 @@
-const noMoreExpensesInCategory = {
+export const noMoreExpensesInCategory = {
     title: 'No More Expenses in this Category',
     description: 'You have explored all the expenses in this category. Start adding new expenses to continue tracking your spending.',
     buttonText: 'Add New Expense',
     buttonLink: '/expenses/create',
+}
+
+export const noBudgetsCreated = {
+    title: 'No Budgets Created',
+    description: 'Start by creating a budget to track your expenses and manage your finances effectively.',
+    buttonText: 'Create New Budget',
+    buttonLink: '/budgets/create',
 }
