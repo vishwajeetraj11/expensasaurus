e6bf2d06a3903c9320cdcd8837cc8532fac55fd8
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:05:35 +0530

    empty state and form UI fixes

--- src/pages/budgets/create.tsx
+++ src/pages/budgets/create.tsx
@@ -4,9 +4,14 @@ import Layout from "expensasaures/components/layout/Layout";
 const create = () => {
   return (
     <Layout>
-      <div className="mx-auto max-w-[1200px] mt-10">
-        <h1 className="text-center mb-10">Create Budget</h1>
-        <BudgetForm />
+      <div className="mx-auto max-w-[1200px] mt-10 w-full flex flex-1 h-full">
+        <div className="w-[50%]">
+          <h1 className="text-center mb-10">Create Budget</h1>
+          <BudgetForm />
+        </div>
+        <div className="bg-blue-500 h-auto w-full flex items-center justify-center flex-1">
+          {/* <Exp /> */}
+        </div>
       </div>
     </Layout>
   );
--- src/pages/budgets/index.tsx
+++ src/pages/budgets/index.tsx
@@ -1,27 +1,31 @@
 import { Button, Title } from "@tremor/react";
+import clsx from "clsx";
 import RecentBudgets from "expensasaures/components/budgets/RecentBudgets";
 import Layout from "expensasaures/components/layout/Layout";
 import useBudgets from "expensasaures/hooks/useBudgets";
+import dynamic from "next/dynamic";
 import Link from "next/link";
+const EmptyStateBudgets = dynamic(() => import("expensasaures/components/budgets/EmptyStateBudgets"), { ssr: false });
 
 const index = () => {
   const { data } = useBudgets();
-
+  const emptyState = data?.total === 0;
   return (
     <Layout>
       <div className="mx-auto max-w-[1200px] w-full">
-        <div className="flex items-center justify-between">
+        <div className={clsx("flex items-center", emptyState ? 'justify-center' : 'justify-between')}>
           &nbsp;
           <Title className="text-center py-10">Budgets</Title>
-          <Link href={"/budgets/create"}>
+          {!emptyState ? <Link href={"/budgets/create"}>
             <Button>Add Budget</Button>
-          </Link>
+          </Link> : null}
         </div>
-        <div>Pinned budget details</div>
+        {emptyState && <EmptyStateBudgets />}
+        {/* <div>Pinned budget details</div>
         <div>Recent Activity</div>
         <div>Completed budgets</div>
-        <div>Failed goals</div>
-        {data && (
+        <div>Failed goals</div> */}
+        {!emptyState && (
           <div>
             <RecentBudgets />
           </div>
