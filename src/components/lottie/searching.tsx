7af2e3fdea9e7f6bf0952631b94c9140c095fbec
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:03:09 +0530

    searching loading state

---
+++ src/components/lottie/searching.tsx
@@ -0,0 +1,37 @@
+import { Metric, Subtitle, Title } from '@tremor/react';
+import animationData from 'expensasaures/lottie/searching.json';
+import { defaultOptions } from "expensasaures/shared/utils/lottie";
+import { useEffect, useState } from 'react';
+import Lottie from "react-lottie";
+
+interface Props {
+    title?: string;
+    description?: string;
+    subtitle?: string;
+}
+
+const Searching = (props: Props) => {
+    const { description, title, subtitle } = props;
+    const [visible, setVisible] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setVisible(true)
+        }, 500)
+        return () => clearTimeout(timeout)
+    }, [])
+    return (
+        <div>
+            <Lottie options={defaultOptions(animationData)}
+                height={'auto'}
+                width={400}
+            />
+            <div className='flex flex-col justify-center items-center'>
+                {title && <Metric className='font-thin mb-2'>{title}</Metric>}
+                {visible && subtitle && <Subtitle className='w-[60%] mb-3 text-center'>{subtitle}</Subtitle>}
+                {visible && description && <Title className='max-w-[60%] text-center'>{description}</Title>}
+            </div>
+        </div>
+    )
+}
+
+export default Searching
