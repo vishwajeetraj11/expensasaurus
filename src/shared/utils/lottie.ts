26205c0de376589c2eb2cb980914068ee981f395
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:04:39 +0530

    lottie default options

---
+++ src/shared/utils/lottie.ts
@@ -0,0 +1,8 @@
+export const defaultOptions = (animationData: object) => ({
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+});
