6444910b01b73b372fed642b1bdfb7f3ca22018e
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 10:05:58 +0530

    date fxi

--- src/components/calender/ExpenseCalCard.tsx
+++ src/components/calender/ExpenseCalCard.tsx
@@ -1,12 +1,11 @@
 import { TrashIcon } from "@heroicons/react/outline";
 import clsx from "clsx";
-import { formatDistance } from "date-fns";
+import { format, formatDistance } from "date-fns";
 import { categories } from "expensasaures/shared/constants/categories";
 import { ENVS } from "expensasaures/shared/constants/constants";
 import { database } from "expensasaures/shared/services/appwrite";
 import { Transaction } from "expensasaures/shared/types/transaction";
 import { timeSince } from "expensasaures/shared/utils/dates";
-import Link from "next/link";
 import { toast } from "sonner";
 interface Props {
   expense: Transaction;
@@ -16,7 +15,7 @@ function ExpenseCalCard(props: Props) {
   const { expense } = props;
   let createdDate = new Date(expense.date);
   const now = new Date();
-  const formattedDate = formatDistance(createdDate, now);
+  const formattedDate = createdDate.getTime() > now.getTime() ? format(createdDate, `d MMMM yy`) : formatDistance(createdDate, now);
   const categoryInfo = categories.find(
     (category) => category.key === expense.category
   );
@@ -36,42 +35,42 @@ function ExpenseCalCard(props: Props) {
   };
 
   return (
-    <Link href={`/expenses/${expense.$id}`}>
-      <li className="flex px-4 py-2 items-center rounded-xl transition-all duration-300 hover:bg-slate-100/80 relative">
-        <div className="py-2 flex flex-1">
-          {SelectedIcon && (
-            <div
-              className={clsx(
-                "min-w-[40px] h-10 bg-opacity-25 rounded-full flex items-center justify-center mr-3",
-                categoryInfo.className
-              )}
-            >
-              <SelectedIcon className="w-5 h-5" />
-            </div>
-          )}
-
-          <div className="w-[65%]">
-            <p className="text-stone-700 font-semibold text-md">
-              {expense.title}
-            </p>
-            <p className="text-stone-600 text-xs font-medium line-clamp-1">
-              {expense.description}
-            </p>
+    // <Link href={`/expenses/${expense.$id}`}>
+    <li className="flex px-4 py-2 items-center rounded-xl transition-all duration-300 hover:bg-slate-100/80 relative">
+      <div className="py-2 flex flex-1">
+        {SelectedIcon && (
+          <div
+            className={clsx(
+              "min-w-[40px] h-10 bg-opacity-25 rounded-full flex items-center justify-center mr-3",
+              categoryInfo.className
+            )}
+          >
+            <SelectedIcon className="w-5 h-5" />
           </div>
-          <div className="ml-auto flex flex-col">
-            <p className="text-xs  text-slate-500 text-right">
-              {timeSince(createdDate) || formattedDate}
-            </p>
-            <div
-              onClick={(e) => onDelete(expense.$id)}
-              className="bg-red-500/30 cursor-pointer mt-auto ml-auto w-5 h-5 rounded-full flex items-center justify-center"
-            >
-              <TrashIcon className="text-red-500 w-3 h-3" />
-            </div>
+        )}
+
+        <div className="w-[65%]">
+          <p className="text-stone-700 font-semibold text-md">
+            {expense.title}
+          </p>
+          <p className="text-stone-600 text-xs font-medium line-clamp-1">
+            {expense.description}
+          </p>
+        </div>
+        <div className="ml-auto flex flex-col">
+          <p className="text-xs  text-slate-500 text-right">
+            {timeSince(createdDate) || formattedDate}
+          </p>
+          <div
+            onClick={(e) => onDelete(expense.$id)}
+            className="bg-red-500/30 cursor-pointer mt-auto ml-auto w-5 h-5 rounded-full flex items-center justify-center"
+          >
+            <TrashIcon className="text-red-500 w-3 h-3" />
           </div>
         </div>
-      </li>
-    </Link>
+      </div>
+    </li>
+    // </Link>
   );
 }
 
