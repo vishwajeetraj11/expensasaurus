4b29782d29899f3a079c0b9d7b6ff52fafb7255d
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:03:02 +0530

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import PageNotFoundLottie from "expensasaures/components/lottie/pageNotFound"
+
+const PageNotFound = () => {
+    return (
+        <PageNotFoundLottie
+            title="404 Page not found"
+            subtitle="Please double-check the URL or navigate back to a valid section of our expense tracker app."
+            description="We're sorry, but the page you are looking for could not be found. It seems you've encountered a missing page on your financial tracking journey. We apologize for any inconvenience caused and want to ensure you have a smooth experience using our expense tracker."
+        />
+    )
+}
+
+export default PageNotFound
