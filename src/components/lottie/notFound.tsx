5680d562d5b602f4914ef9609e30354b1eef3f5c
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 21:02:57 +0530

    404 and page not found

---
+++ src/components/lottie/notFound.tsx
@@ -0,0 +1,37 @@
+import { Button, Metric, Subtitle, Title } from '@tremor/react';
+import animationData from 'expensasaures/lottie/emptyBox.json';
+import { defaultOptions } from "expensasaures/shared/utils/lottie";
+import Link from 'next/link';
+import Lottie from "react-lottie";
+
+interface Props {
+    title?: string;
+    description?: string;
+    subtitle?: string;
+    href?: string;
+    onClick?: () => void;
+    btnCTA?: string;
+}
+
+const NotFound = (props: Props) => {
+    const { description, title, subtitle, btnCTA, href, onClick = () => { } } = props;
+
+    return (
+        <div className='mt-5 mb-5 sm:mt-20'>
+            <Lottie options={defaultOptions(animationData)}
+                height={'auto'}
+                width={400}
+            />
+            <div className='flex flex-col justify-center items-center px-4'>
+                {title && <Metric className='font-thin mb-3'>{title}</Metric>}
+                {subtitle && <Subtitle className='sm:w-[60%] mb-5 text-center'>{subtitle}</Subtitle>}
+                {description && <Title className='sm:max-w-[60%] text-center'>{description}</Title>}
+                {href && <Link href={href}>
+                    <Button onClick={onClick} color='slate' className='mt-5'>{btnCTA}</Button>
+                </Link>}
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
---
+++ src/components/lottie/pageNotFound.tsx
@@ -0,0 +1,38 @@
+import { Button, Flex, Metric, Subtitle, Title } from '@tremor/react';
+import animationData from 'expensasaures/lottie/pageNotFound.json';
+import { defaultOptions } from "expensasaures/shared/utils/lottie";
+import Link from 'next/link';
+import Lottie from "react-lottie";
+
+interface Props {
+    title?: string;
+    description?: string;
+    subtitle?: string;
+}
+
+
+const PageNotFoundLottie = (props: Props) => {
+    const { description, title, subtitle } = props;
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center px-4 py-8">
+            {title && <Metric className='font-thin mb-20 order-1'>{title}</Metric>}
+            <div className='mb-20 h-[200px] md:w-auto md:h-[40vh] order-2'>
+                <Lottie options={defaultOptions(animationData)}
+                    width={'100%'}
+                />
+            </div>
+            {subtitle && <Subtitle className='order-3 sm:w-[60%] mb-3 text-center text-slate-500'>{subtitle}</Subtitle>}
+            {description && <Title className='order-4 sm:max-w-[60%] text-center font-normal'>{description}</Title>}
+            <Flex className='order-5 max-w-[280px] mt-10'>
+                <Link href={'/dashboard'}>
+                    <Button color='slate' variant='primary'>Dashboard</Button>
+                </Link>
+                <Link href={'/dashboard'}>
+                    <Button color='slate' variant='secondary'>Explore Expenses</Button>
+                </Link>
+            </Flex>
+        </div>
+    )
+}
+
+export default PageNotFoundLottie
