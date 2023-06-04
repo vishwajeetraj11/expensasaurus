c4a130648223d8ed3698ce199c7b621eb27f7997
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-04 12:20:30 +0530

    demo data for loading charts

---
+++ src/shared/constants/loadingData.tsx
@@ -0,0 +1,74 @@
+export const demoDashboardLineChart = [{
+    date: "",
+    amount: 100,
+},
+{
+    date: "",
+    amount: 150,
+},
+{
+    date: "",
+    amount: 20,
+},
+{
+    date: "",
+    amount: 50,
+},
+{
+    date: "",
+    amount: 80,
+},
+{
+    date: "",
+    amount: 60,
+},
+{
+    date: "",
+    amount: 20,
+},
+{
+    date: "",
+    amount: 100,
+},
+{
+    date: "",
+    amount: 45,
+},
+{
+    date: "",
+    amount: 78,
+},
+{
+    date: "",
+    amount: 29,
+},
+{
+    date: "",
+    amount: 40,
+},
+{
+    date: "",
+    amount: 28,
+}, {
+    date: "",
+    amount: 49,
+}, {
+    date: "",
+    amount: 56,
+}, {
+    date: "",
+    amount: 23,
+}, {
+    date: "",
+    amount: 19,
+}, {
+    date: "",
+    amount: 89,
+}, {
+    date: "",
+    amount: 43,
+}, {
+    date: "",
+    amount: 67,
+},
+]
