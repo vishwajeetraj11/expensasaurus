25a79b32dfc80eb4ad69e4a4efdfd2038e8ba0a8
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-09 19:55:46 +0530

    empty state responsive

--- src/components/budgets/EmptyStateBudgets.tsx
+++ src/components/budgets/EmptyStateBudgets.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@mui/material";
 import { Button, Metric, Subtitle } from "@tremor/react";
 import animationData from 'expensasaures/lottie/piggyBank.json';
 import { noBudgetsCreated } from "expensasaures/shared/constants/emptyState";
@@ -7,18 +8,19 @@ import Lottie from 'react-lottie';
 
 
 const EmptyStateBudgets = () => {
+    const isMobile = useMediaQuery('(max-width: 600px)');
     return (
-        <div className="bg-slate-100 flex items-center p-6 min-h-[400px] rounded-md">
-            <div className="ml-10 flex flex-col">
-                <Metric className="text-slate-600 font-thin">{noBudgetsCreated.title}</Metric>
-                <Subtitle className="mt-3 mb-4 max-w-[280px] font-medium text-slate-500">{noBudgetsCreated.description}</Subtitle>
+        <div className="bg-slate-100 flex gap-10 flex-col-reverse md:flex-row items-center p-6 min-h-[400px] rounded-md">
+            <div className="ml-0 md:ml-10 flex flex-col items-center md:items-start">
+                <Metric className="text-center md:text-left text-slate-600 font-thin">{noBudgetsCreated.title}</Metric>
+                <Subtitle className="text-center md:text-left mt-3 mb-4 max-w-[280px] font-medium text-slate-500">{noBudgetsCreated.description}</Subtitle>
                 <Link shallow prefetch={false} href={noBudgetsCreated.buttonLink}>
                     <Button>+ {noBudgetsCreated.buttonText}</Button>
                 </Link>
             </div>
             <Lottie options={defaultOptions(animationData)}
-                height={400}
                 width={'auto'}
+                height={isMobile ? 'auto' : '400px'}
             />
         </div>
     )
