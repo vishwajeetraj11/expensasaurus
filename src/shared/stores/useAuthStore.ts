6b186284879b3782efae3d3a9dd2773c9de6bc53
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-09 09:59:38 +0530

    logout method

--- src/shared/stores/useAuthStore.ts
+++ src/shared/stores/useAuthStore.ts
@@ -1,4 +1,5 @@
 import { Models } from 'appwrite'
+import { NextRouter } from 'next/router'
 import { create } from 'zustand'
 import { account } from '../services/appwrite'
 import { authFormStateType } from '../types/auth'
@@ -11,7 +12,8 @@ type useAuthStoreType = {
     getUser: () => Promise<Models.Session | undefined>
     userInfo: Models.User<Models.Preferences> | null,
     setUserInfo: (user: Models.User<Models.Preferences> | null) => void,
-    getUserInfo: () => Promise<void>
+    getUserInfo: () => Promise<void>;
+    logout: (router: NextRouter) => Promise<void>;
 }
 
 const useAuthStore = create<useAuthStoreType>((set, get) => ({
@@ -21,6 +23,17 @@ const useAuthStore = create<useAuthStoreType>((set, get) => ({
     user: null,
     setUser: (user) => set({ user }),
     setUserInfo: (userInfo) => set({ userInfo }),
+    logout: async (router: NextRouter) => {
+        try {
+            const { user, setUser } = get();
+            if (user) {
+                await account.deleteSession(user.$id);
+                localStorage.removeItem("sessionId");
+                setUser(null);
+                // router.push('/login')
+            }
+        } catch (e) { }
+    },
     getUser: async () => {
         try {
             const { user } = get();
@@ -36,6 +49,10 @@ const useAuthStore = create<useAuthStoreType>((set, get) => ({
                         set({ user: currentUser });
                         return currentUser;
                     }
+                    set({ user: currentUser });
+                    return currentUser;
+                } else {
+                    const currentUser = await account.getSession("current");
                     set({ user: currentUser });
                     return currentUser;
                 }
