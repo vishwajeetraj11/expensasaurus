2d005985e7d3cc543f852eac56a208d272841431
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-10 17:00:46 +0530

    minot

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { HTMLProps } from "react";
+import ErrorMessage from "./ErrorMessage";
 
 interface InputFieldProps extends HTMLProps<HTMLInputElement> {
   label: string;
@@ -30,9 +31,8 @@ function InputField(props: InputFieldProps) {
     <div className={`${extra}`}>
       <label
         htmlFor={id}
-        className={`text-sm text-navy-700 dark:text-white ${
-          isAuth ? "ml-1.5 font-medium" : "ml-3 font-bold"
-        }`}
+        className={`text-sm text-navy-700 dark:text-white ${isAuth ? "ml-1.5 font-medium" : "ml-3 font-bold"
+          }`}
       >
         {label}
       </label>
@@ -42,22 +42,21 @@ function InputField(props: InputFieldProps) {
         id={id}
         placeholder={placeholder}
         className={clsx(
-          `mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none ${
-            disabled === true
-              ? "!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255,255,255,0.15)]"
-              : isError
+          `mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none ${disabled === true
+            ? "!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255,255,255,0.15)]"
+            : isError
               ? "border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400"
               : state === "success"
-              ? "border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400"
-              : "border-gray-200 dark:!border-white/10 dark:text-white"
+                ? "border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400"
+                : "border-gray-200 dark:!border-white/10 dark:text-white"
           }`
         )}
         {...rest}
       />
       {message && (
-        <p className="text-red-600 dark:text-red-400 text-xs mt-1.5 tracking-normal">
+        <ErrorMessage>
           {message}
-        </p>
+        </ErrorMessage>
       )}
     </div>
   );
