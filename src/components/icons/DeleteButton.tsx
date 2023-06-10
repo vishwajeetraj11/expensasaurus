93d694185612363921c56a57eb72fa931e96cb77
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-10 16:22:48 +0530

    delete button ui fix

--- src/components/icons/DeleteButton.tsx
+++ src/components/icons/DeleteButton.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 interface DeleteButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   size?: "sm" | "md" | "lg";
+  disabled?: boolean;
 }
 
 const DeleteButton = (props: DeleteButtonProps) => {
@@ -12,8 +13,8 @@ const DeleteButton = (props: DeleteButtonProps) => {
   const isSmall = size === 'sm';
   const classes = clsx(
     "bg-red-600/10 hover:bg-red-600/20 transition-all duration-200 rounded-full flex items-center justify-center",
-    isLarge && "w-8 h-8",
-    isSmall && "w-5 h-5",
+    isLarge && "w-8 h-8 min-w-[32px]",
+    isSmall && "w-5 h-5 min-w-[20px]",
     className
   );
   const iconClasses = clsx("text-red-600", isLarge ? "w-4 h-4" : "w-3 h-3")
