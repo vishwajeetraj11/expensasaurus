2925e35b6a182fc40bddf7b33632d2b6a5816a7e
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-10 17:00:24 +0530

    text fix

--- src/components/lottie/searching.tsx
+++ src/components/lottie/searching.tsx
@@ -26,11 +26,11 @@ const Searching = (props: Props) => {
                 width={400}
             />
             <div className='flex flex-col justify-center items-center'>
-                {title && <Metric className='font-thin mb-2'>{title}</Metric>}
+                {title && <Metric className='font-thin text-slate-500 mb-2'>{title}</Metric>}
                 {/* {visible && subtitle && <Subtitle className='w-[60%] mb-3 text-center'>{subtitle}</Subtitle>} */}
                 {subtitle && <Subtitle className='w-[60%] mb-3 text-center'>{subtitle}</Subtitle>}
                 {/* {visible && description && <Title className='max-w-[60%] text-center'>{description}</Title>} */}
-                {description && <Title className='max-w-[60%] text-center'>{description}</Title>}
+                {description && <Title className='max-w-[60%] text-slate-500 text-center'>{description}</Title>}
             </div>
         </div>
     )
