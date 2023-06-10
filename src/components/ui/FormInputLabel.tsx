6e9369874e4156f76c478a8d0da0f19782af76e9
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-10 19:35:10 +0530

    dark mode fix for form label

--- src/components/ui/FormInputLabel.tsx
+++ src/components/ui/FormInputLabel.tsx
@@ -7,7 +7,7 @@ interface LabelProps extends HTMLProps<HTMLLabelElement> {
 
 const FormInputLabel = (props: LabelProps) => {
   const { children, className } = props;
-  const defaultClasses = clsx(className, 'mb-2 block text-[14px]')
+  const defaultClasses = clsx(className, 'dark:text-slate-200 mb-2 block text-[14px]')
   return <label {...props} className={defaultClasses}>{children}</label>;
 };
 
