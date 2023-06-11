260750f6f52b1ca052f8cdbca6bcf86477418b32
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-11 13:39:03 +0530

    landing page fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Navigation from "expensasaures/components/Navigation";
+import Footer from "expensasaures/components/landing/footer";
+
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`flex min-h-screen flex-col items-center justify-between`}
     >
-      Landing Page
+      <div className="p-24">
+        <Navigation landingPage />
+      </div>
+
+      <Footer />
+
     </main>
   );
 }
