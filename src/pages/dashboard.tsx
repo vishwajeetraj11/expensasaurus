263fdd6567533670de99511f006a2338e861091a
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-06-12 00:18:15 +0530

    dashboard title tag

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,6 +4,7 @@ import LineChartTabs, { LineChartTabsLoading } from "expensasaures/components/da
 import DashboardStatistics, { DashboardStatisticsLoading } from "expensasaures/components/dashboard/stats";
 import Layout from "expensasaures/components/layout/Layout";
 import useDashboard from "expensasaures/hooks/useDashboard";
+import Head from "next/head";
 
 const dashboard = () => {
   const {
@@ -17,6 +18,9 @@ const dashboard = () => {
   // const isLoading = true;
   return (
     <Layout>
+      <Head>
+        <title>Expensasaures - Monthly Performance Dashboard</title>
+      </Head>
       <main className="max-w-[1200px] w-full mx-auto pt-10 px-4">
         <Title className="font-thin text-center mb-10">Monthly Performance Dashboard</Title>
         {isLoading ? <DashboardStatisticsLoading /> : <DashboardStatistics stats={statistics} />}
