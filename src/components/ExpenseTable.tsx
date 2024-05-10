0d32f7dd72eb32fabfb451ad208bc53d3fad417b
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-11 03:00:08 +0530

    feat: upgrade tremor

--- src/components/ExpenseTable.tsx
+++ src/components/ExpenseTable.tsx
@@ -1,8 +1,6 @@
 import {
-  Column,
   ColumnDef,
   PaginationState,
-  Table,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
@@ -144,7 +142,6 @@ function Table({
 
   return (
     <div className="flex flex-col flex-1">
-
       <div className="p-2 flex flex-col flex-1 overflow-x-scroll">
         <table className="w-full">
           <thead>
@@ -198,7 +195,6 @@ function Table({
             })}
           </tbody>
         </table>
-
       </div>
       <hr className="my-4 mt-auto" />
       <div className="flex items-center justify-between gap-2">
@@ -277,57 +273,6 @@ function Table({
         </div>
       </div>
     </div>
-  );
-}
-
-function Filter({
-  column,
-  table,
-}: {
-  column: Column<any, any>;
-  table: Table<any>;
-}) {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id);
-
-  const columnFilterValue = column.getFilterValue();
-
-  return typeof firstValue === "number" ? (
-    <div className="flex space-x-2">
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ])
-        }
-        placeholder={`Min`}
-        className="w-24 border shadow rounded"
-      />
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ])
-        }
-        placeholder={`Max`}
-        className="w-24 border shadow rounded"
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={(columnFilterValue ?? "") as string}
-      onChange={(e) => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
   );
 }
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import { Title } from "@tremor/react";
-import CategoriesPieChart, { CategoriesPieChartLoading } from "expensasaurus/components/dashboard/categorypiechart";
-import LineChartTabs, { LineChartTabsLoading } from "expensasaurus/components/dashboard/linechart";
-import DashboardStatistics, { DashboardStatisticsLoading } from "expensasaurus/components/dashboard/stats";
+import CategoriesPieChart, {
+  CategoriesPieChartLoading,
+} from "expensasaurus/components/dashboard/categorypiechart";
+import LineChartTabs, {
+  LineChartTabsLoading,
+} from "expensasaurus/components/dashboard/linechart";
+import DashboardStatistics, {
+  DashboardStatisticsLoading,
+} from "expensasaurus/components/dashboard/stats";
 import Layout from "expensasaurus/components/layout/Layout";
 import useDashboard from "expensasaurus/hooks/useDashboard";
 import Head from "next/head";
@@ -22,27 +28,39 @@ const dashboard = () => {
         <title>Expensasaurus - Monthly Performance Dashboard</title>
       </Head>
       <main className="max-w-[1200px] w-full mx-auto pt-10 px-4">
-        <Title className="font-thin text-center mb-10">Monthly Performance Dashboard</Title>
-        {isLoading ? <DashboardStatisticsLoading /> : <DashboardStatistics stats={statistics} />}
+        <Title className="font-thin text-center mb-10">
+          Monthly Performance Dashboard
+        </Title>
+        {isLoading ? (
+          <DashboardStatisticsLoading />
+        ) : (
+          <DashboardStatistics stats={statistics} />
+        )}
 
         <div className="flex flex-col md:flex-row justify-between gap-4 mt-8">
           <div className="w-full md:w-[66%]">
-            {isLoading ? <>
-              <LineChartTabsLoading />
-            </> : <LineChartTabs />}
-
+            {isLoading ? (
+              <>
+                <LineChartTabsLoading />
+              </>
+            ) : (
+              <LineChartTabs />
+            )}
           </div>
           <div className="w-full md:w-[32%]">
-            {isLoading ? <CategoriesPieChartLoading /> : <CategoriesPieChart
-              incomeThisMonth={incomeThisMonth}
-              expensesAndPercentByCategoryThisMonth={
-                expensesAndPercentByCategoryThisMonth
-              }
-              expenseThisMonth={expenseThisMonth}
-            />}
+            {isLoading ? (
+              <CategoriesPieChartLoading />
+            ) : (
+              <CategoriesPieChart
+                incomeThisMonth={incomeThisMonth}
+                expensesAndPercentByCategoryThisMonth={
+                  expensesAndPercentByCategoryThisMonth
+                }
+                expenseThisMonth={expenseThisMonth}
+              />
+            )}
           </div>
         </div>
-
       </main>
     </Layout>
   );
